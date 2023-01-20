5a0a864197278052626f0c16e966d8038b2b6554
Edgar Méndez <mdzredgar@gmail.com>
2023-01-20 15:56:54 -0600

    Elementos centrados.

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -2,7 +2,7 @@ import QuoteButton from "./QuoteButton"
 
 const QuoteBox = ({quote, newQuote}) => {
   return (
-    <div>
+    <div className="quote-box">
       <p>{quote.quote}</p>
       <h3>{quote.author}</h3>
       <i className='bx bxs-quote-alt-left'></i>
